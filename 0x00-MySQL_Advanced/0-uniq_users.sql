222812e9e2516af27bd131c7239250a253b411dc
MaryKwawu <may419316@gmail.com>
2023-10-12 22:39:19 +0000

    added mysql files

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,17 +1,6 @@
 /*
-This script creates a table named 'users' in a database.
-The table has three columns:
-- 'id': an integer that is never null, auto-increments, and serves as the primary key.
-- 'email': a string of up to 255 characters that is never null and must be unique.
-- 'name': a string of up to 255 characters.
-
-The script checks if the table already exists in the database before attempting to create it. 
-This prevents the script from failing if the table already exists.
-
-This script is written in SQL and should work on any database that supports SQL.
+This script creates a table named 'users' in a databas
 */
-
--- MySQL
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
